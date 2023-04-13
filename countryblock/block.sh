b23411b30d8d24bf6fd6f100bf9eec7b0b570cd9
Alec <alec.minch@gmail.com>
2023-04-12 19:20:12 -0700

    nftables

--- countryblock/block.sh
+++ countryblock/block.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # countryblock script for docker
-# <scriptname> start will set up iptables and download the specified country ipsets and wait
-# until it receives a INT, TERM, or KILL signal, at which time it will clean up iptables
+# <scriptname> start will set up nftables and download the specified country ipsets and wait
+# until it receives a INT, TERM, or KILL signal, at which time it will clean up nftables
 # <scriptname> update will update the ipsets, good for a cron job
 # Copyright (C) 2020 Bradford Law
 # Licensed under the terms of MIT
@@ -21,48 +21,40 @@ CHAIN=countryblock
 
 printf "Starting blocklist and ipset construction for countries: %b\n" "$COUNTRIES" >> $LOG
 
-# Reused iptables rules
-FORWARD_RULE="FORWARD -j $CHAIN"
-
-
 setup() {
-	# Create chain and RETURN and FORWARD rules
-	iptables -N $CHAIN
-	iptables -A $CHAIN -j RETURN
-	iptables -I $FORWARD_RULE
+	# Create table and chain
+	nft add table ip filter
+	nft add chain ip filter $CHAIN { type filter hook forward priority 0 \; }
 
 	for country in $COUNTRIES; do
 		COUNTRY_LOWER=${country,,}
 
-		# Create ipset for each country
-		ipset -exist create $COUNTRY_LOWER hash:net
+		# Create set for each country
+		nft add set ip filter $COUNTRY_LOWER { type ipv4_addr \; }
 
 		# Create firewall rule for each country
-		iptables -I $CHAIN -m set --match-set $COUNTRY_LOWER src -j DROP
+		nft add rule ip filter $CHAIN ip saddr @$COUNTRY_LOWER drop
 	done
-	printf "Created %b chain and rules and ipsets for countries %b\n" "$CHAIN" "$COUNTRIES" >> $LOG
-
+	printf "Created %b chain and rules and sets for countries %b\n" "$CHAIN" "$COUNTRIES" >> $LOG
 }
 
 cleanup() {
+	# Clean up rules and sets
+	for country in $COUNTRIES; do
+		COUNTRY_LOWER=${country,,}
+		nft delete rule ip filter $CHAIN ip saddr @$COUNTRY_LOWER drop
+		nft delete set ip filter $COUNTRY_LOWER
+	done
 
-	# Clean up old rules
-	iptables -D $FORWARD_RULE
-	iptables -F $CHAIN
-	iptables -X $CHAIN
+	# Remove chain and table
+	nft delete chain ip filter $CHAIN
+	nft delete table ip filter
 
-	# Flush ipsets
-        for country in $COUNTRIES; do
-                COUNTRY_LOWER=`echo "$country" | tr '[:upper:]' '[:lower:]'`
-                # Flush ipset for each country
-                ipset flush $COUNTRY_LOWER
-        done
-	printf "Removed %b chain and rules and flushed ipsets\n" "$CHAIN" >> $LOG
+	printf "Removed %b chain and rules and sets\n" "$CHAIN"
 }
 
 update() {
-	# For each country, download a list of subnets and add to its respective ipset
-	# https://askubuntu.com/a/931153/56882 was useful
+	# For each country, download a list of subnets and add to its respective set
 	for country in $COUNTRIES; do
 		COUNTRY_LOWER=${country,,}
 
@@ -71,12 +63,10 @@ update() {
 		wget --no-check-certificate -N https://www.ipdeny.com/ipblocks/data/aggregated/$ZONEFILE
 		printf "Downloaded zone file for %b\n" "$country" >> $LOG
 
-		# Add each IP address from the downloaded list into the ipset 'china'
-		for i in $(cat $ZONEFILE ); do ipset -exist -A $COUNTRY_LOWER $i; done
-		printf "Added %b subnets to %b ipset\n" "$(wc -l $ZONEFILE)" "$country" >> $LOG
-
+		# Add each IP address from the downloaded list into the set
+		for i in $(cat $ZONEFILE ); do nft add element ip filter $COUNTRY_LOWER { $i \; } ; done
+		printf "Added %b subnets to %b set\n" "$(wc -l $ZONEFILE)" "$country" >> $LOG
 	done
-
 }
 
 if [ "$1" == "start" ]; then
@@ -92,6 +82,6 @@ if [ "$1" == "start" ]; then
 	wait
 
 elif [ "$1" == "update" ]; then
-	# Update the ipsets and exit
+	# Update the sets and exit
 	update
 fi
