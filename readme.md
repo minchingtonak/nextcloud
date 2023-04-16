dba681d8a5f67c755074457e4cb501fb5d47b397
Alec <alec.minch@gmail.com>
2023-04-15 17:14:26 -0700

    Add fail2ban, readme

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Setup
+
+1. Run `use-legacy-iptables.sh`
+2. Run `docker compose up -d`
---
+++ use-legacy-iptables.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# For debian systems only
+
+set -Eeuo pipefail
+
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+update-alternatives --set arptables /usr/sbin/arptables-legacy
+update-alternatives --set ebtables /usr/sbin/ebtables-legacy
